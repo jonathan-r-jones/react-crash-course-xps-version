aa6e294a73a84b6241f0d790a320f0f0634adeed
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2021-10-12 15:00:36 -0400

    Oct-12-2021_3:00_PM

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,8 +4,9 @@ const Task = ({ task, onDelete }) => {
   return (
     <div className='task'>
       <h3>
-        {task.text} <FaTimes style={{ color: 'red', cursor: 'pointer' }}
-        onClick={onDelete}/>
+        {task.text} <FaTimes 
+        style={{ color: 'red', cursor: 'pointer' }}
+        onClick={() => onDelete(task.id)}/>
       </h3>
       <p>{task.day}</p>
     </div>
