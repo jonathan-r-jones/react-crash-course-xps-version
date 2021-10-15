792afcb73fc6272a998b04aee5246a25d821c65a
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2021-10-15 16:42:49 -0400

    Oct-15-2021_4:42_PM

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import AddTask from './components/AddTask'
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+    getTasks()
+  }, [])
+
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    return data
+  }
 
   const name = 'Brad'
   const x = false
